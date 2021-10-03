e47e00fbd8a648e0428556fc84403a88866eaa67
OjijuanHaman <ojijuan.haman@bryanuniversity.edu>
2021-10-03 14:36:16 -0500

    added week1 assignements

---
+++ Week1/E-CommerceStore/models/inventory.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+const schema = mongoose.Schema
+
+const inventorySchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required:true,
+        min: 0
+    }
+})
+
+module.exports = mongoose.model('Inventory Item', inventorySchema)
